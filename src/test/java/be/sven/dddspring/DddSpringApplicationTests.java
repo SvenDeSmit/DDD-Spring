9a30cf8bc4c1934f5aa14bc11ba0cc19388d3af5
SvenDeSmit <sven.desmit@telenet.be>
2022-08-28 18:49:05 +0200

    First commit

---
+++ src/test/java/be/sven/dddspring/DddSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package be.sven.dddspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
